45de8550597dfde8f4f4fd877a874fb5a62fc1de
Andre <anddreluis98@gmail.com>
2022-09-03 02:09:26 -0300

    ESLINT CONFIG

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const { application } = require('express');
 const express = require('express');
 
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send("HELLO WORLD!");
+  response.send('HELLO WORLD!');
 });
 
-app.listen(3000, () => console.log("🔥 server started at http://localhost:3000"));
+app.listen(3000, () => console.log('🔥 server started at http://localhost:3000'));
